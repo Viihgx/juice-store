c67cd6b19d9b6eafd27f20055755cfb06b7a2334
Viihgx <vitoriasamara.g77@gmail.com>
2024-07-10 20:58:02 -0300

    :sparkles: Add Logotipo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,19 +3,22 @@ import { Navbar, Nav, Badge } from 'react-bootstrap';
 import { FaShoppingCart } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import './Header.css';
+import LogoImage from '../assets/images/LogoJuice.png';
 
 function Header({ cartCount }) {
   return (
     <Navbar bg="light" expand="lg" fixed="top" className="header">
-      <Navbar.Brand as={Link} to="/" className="logo">JuiceShop</Navbar.Brand>
+      <Navbar.Brand href="#hero" className="logo">
+        <img src={LogoImage} alt="JuiceShop Logo" className="logo-image" />
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mx-auto">
-          <Nav.Link as={Link} to="/#hero" className="nav-link">Home</Nav.Link>
-          <Nav.Link as={Link} to="/#introduction" className="nav-link">Sobre Nós</Nav.Link>
-          <Nav.Link as={Link} to="/#benefits" className="nav-link">Benefícios</Nav.Link>
-          <Nav.Link as={Link} to="/#products" className="nav-link">Produtos</Nav.Link>
-          <Nav.Link as={Link} to="/#contact" className="nav-link">Contato</Nav.Link>
+          <Nav.Link href="#hero" className="nav-link">Home</Nav.Link>
+          <Nav.Link href="#introduction" className="nav-link">Sobre Nós</Nav.Link>
+          <Nav.Link href="#benefits" className="nav-link">Benefícios</Nav.Link>
+          <Nav.Link href="#products" className="nav-link">Produtos</Nav.Link>
+          <Nav.Link href="#contact" className="nav-link">Contato</Nav.Link>
         </Nav>
         <Nav className="ml-auto">
           <Nav.Link as={Link} to="/cart" className="nav-icon">
