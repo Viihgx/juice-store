2d88c5078c085cebabe3b47adeab4aa33e485b12
Viihgx <vitoriasamara.g77@gmail.com>
2024-07-10 21:20:14 -0300

    :art: adjust menu links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,36 @@
 import React from 'react';
 import { Navbar, Nav, Badge } from 'react-bootstrap';
 import { FaShoppingCart } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Header.css';
 import LogoImage from '../assets/images/LogoJuice.png';
 
 function Header({ cartCount }) {
+  const navigate = useNavigate();
+
+  const handleNavClick = (sectionId) => {
+    navigate('/');
+    setTimeout(() => {
+      const section = document.querySelector(sectionId);
+      if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+      }
+    }, 100);
+  };
+
   return (
     <Navbar bg="light" expand="lg" fixed="top" className="header">
-      <Navbar.Brand href="#hero" className="logo">
+      <Navbar.Brand className="logo" onClick={() => handleNavClick('#hero')}>
         <img src={LogoImage} alt="JuiceShop Logo" className="logo-image" />
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mx-auto">
-          <Nav.Link href="#hero" className="nav-link">Home</Nav.Link>
-          <Nav.Link href="#introduction" className="nav-link">Sobre Nós</Nav.Link>
-          <Nav.Link href="#benefits" className="nav-link">Benefícios</Nav.Link>
-          <Nav.Link href="#products" className="nav-link">Produtos</Nav.Link>
-          <Nav.Link href="#contact" className="nav-link">Contato</Nav.Link>
+          <Nav.Link onClick={() => handleNavClick('#hero')} className="nav-link">Home</Nav.Link>
+          <Nav.Link onClick={() => handleNavClick('#introduction')} className="nav-link">Sobre Nós</Nav.Link>
+          <Nav.Link onClick={() => handleNavClick('#benefits')} className="nav-link">Benefícios</Nav.Link>
+          <Nav.Link onClick={() => handleNavClick('#products')} className="nav-link">Produtos</Nav.Link>
+          <Nav.Link onClick={() => handleNavClick('#contact')} className="nav-link">Contato</Nav.Link>
         </Nav>
         <Nav className="ml-auto">
           <Nav.Link as={Link} to="/cart" className="nav-icon">
