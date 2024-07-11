b1e26264992573f05082fdcb900a4d1bdee1e187
Viihgx <vitoriasamara.g77@gmail.com>
2024-07-10 23:26:00 -0300

    sparkles: add contact us

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,21 +1,47 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import { FaLeaf, FaHeartbeat } from 'react-icons/fa';
+import './Contact.css';
 
 function Contact() {
   return (
     <div className="contact" id="contact">
-      <Container>
-        <Row>
-          <Col md={6}>
-            <h5>Contact Us</h5>
-            <p>Email: info@juiceshop.com</p>
-            <p>Phone: +1234567890</p>
-          </Col>
-          <Col md={6}>
-            <h5>Find Us</h5>
-            <p>123 Juice Street, Flavor Town</p>
+      <Container fluid className="contact-container">
+        <Row className="banner-row">
+          <Col className="banner">
+            <Row className="d-flex justify-content-center align-items-center text-center">
+              <Col xs={6} md={4} className="banner-item">
+                <FaLeaf className="banner-icon" />
+                <span className="banner-text">Suco Natural</span>
+              </Col>
+              <Col xs={6} md={4} className="banner-item">
+                <FaHeartbeat className="banner-icon" />
+                <span className="banner-text">Sem Calorias</span>
+              </Col>
+            </Row>
           </Col>
         </Row>
+        <div className="contact-box">
+          <Row className="contact-row">
+            <Col xs={12}>
+              <h5 className="contact-title">Nossos Contatos:</h5>
+            </Col>
+          </Row>
+          <Row className="contact-row">
+            <Col xs={12} md={6} className="contact-info">
+              <h6>Escreva para nós</h6>
+              <p>
+                <a href="mailto:info@sucopuro.com">info@sucopuro.com</a>
+              </p>
+            </Col>
+            <Col xs={12} md={6} className="contact-info">
+              <h6>Ligue para nós</h6>
+              <p>+85-999-999-999 <br /> +85 3000.3000</p>
+              <h6>Nos encontre aqui</h6>
+              <p>Rua dos Sabores, 123 <br /> Cidade do Sabor, Brasil</p>
+            </Col>
+          </Row>
+        </div>
       </Container>
     </div>
   );
